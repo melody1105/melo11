d79144be3a9726ebee2687e60e5754065afc90ae
Melody <melody110595@gmail.com>
2019-04-03 13:49:07 +0200

    message

--- hello.py
+++ hello.py
@@ -4,10 +4,20 @@ Declare var
 Store some arithmetics into the var
 Display result
 Operands and operators, functions
+
+@version 0.0.2
+Add two operands and replace compute method
 """
 resultat = 0 # Définition de la variable
-resultat = 2 + 2
+operande1 = -3
+operande2 = 2
+resultat = operande1 + operande2
 print(resultat)
+
+if resultat > 0:
+    print("Resultat positif")
+else:
+    print ("Resultat négatif")
 
 """
 Fin de l'algorithme
